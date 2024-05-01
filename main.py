cdbb6175f927bb46e2416949f1166faf6dc8eabf
Seiya David <seiya.david@yahoo.co.uk>
2024-05-01 03:24:50 +0100

    updated main, pipeline

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from src.riskdemo import logger
+from src.riskDemo import logger
+from src.riskDemo.pipeline.initial_data_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
 
-logger.info("Welcome to our custom logging")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
