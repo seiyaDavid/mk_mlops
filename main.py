98887656f4682c4d1ebb685810349d686cb27502
Seiya David <seiya.david@yahoo.co.uk>
2024-05-01 04:50:02 +0100

    data validation added

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ from src.riskDemo.pipeline.initial_data_data_ingestion import (
     DataIngestionTrainingPipeline,
 )
 
+from src.riskDemo.pipeline.data_val import DataValidationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion "
 try:
     logger.info(f"=========== {STAGE_NAME} process has started ===========")
     data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"=========== {STAGE_NAME} process is finished ===========\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation"
+try:
+    logger.info(f"=========== {STAGE_NAME} process has started ===========")
+    data_ingestion = DataValidationTrainingPipeline()
     data_ingestion.main()
     logger.info(f"=========== {STAGE_NAME} process is finished ===========\n\n")
 except Exception as e:
